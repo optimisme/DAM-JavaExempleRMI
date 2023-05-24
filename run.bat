7b7f120e61d4debe75ef61c9dcb0d9bbe9835f33
optimisme <optimisme@gmail.com>
2023-05-24 16:44:38 +0200

    Updated .bat

--- run.bat
+++ run.bat
@@ -1,30 +1,30 @@
-rem run with: .\run.bat
+@echo off
 
-cls
+REM Remove any existing Project.jar file
+if exist .\release rmdir /s /q .\release
 
-rem Remove any existing Project.jar file
-del Project.jar
+REM Remove any existing .class files from the bin directory
+if exist .\bin rmdir /s /q .\bin
 
-rem Remove any existing .class files from the bin directory
-del /q bin\*.*
+REM Create the bin directory if it doesn't exist
+if not exist .\bin mkdir .\bin
 
-rem Create the bin directory if it doesn't exist
-mkdir bin
+REM Copy the assets directory to the bin directory
+xcopy /E /I .\assets .\bin\assets
 
-rem Copy the assets directory to the bin directory
-xcopy assets bin\assets /E /I /Y
+REM Compile the Java source files and place the .class files in the bin directory
+javac -d .\bin\ .\src\*.java
 
-rem Compile the Java source files and place the .class files in the bin directory
-javac -d bin src\*.java
+REM Create the Project.jar file with the specified manifest file and the contents of the bin directory
+echo Main-Class: Main > .\Manifest.txt
+echo Class-Path: . >> .\Manifest.txt
+jar cfm .\release\Project.jar .\Manifest.txt -C .\bin\ .
+del .\Manifest.txt
 
-rem Create the Project.jar file with the specified manifest file and the contents of the bin directory
-jar cfm release\Project.jar src\Manifest.txt -C bin .
+REM Remove any .class files from the bin directory
+if exist .\bin rmdir /s /q .\bin
 
-rem Remove any .class files from the bin directory
-del /q bin\*.*
-rmdir bin
-
-rem Run the Project.jar file
-cd release
+REM Run the Project.jar file
+cd .\release
 java -jar Project.jar
 cd ..
